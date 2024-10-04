62d9f07de25efbe63cd6760e98ffcdf431f0c0f1
Kristina-K12 <kocakova.kristina@gmail.com>
2024-10-04 13:24:40 +0200

    cleaned ans simplified code

--- code/Fig5_chronostratigraphic_distribution.py
+++ code/Fig5_chronostratigraphic_distribution.py
@@ -7,9 +7,7 @@ Plotting of distribution of occurrences through epochs and stages
 """
 
 from pandas import *
-import numpy as np
 import matplotlib.pyplot as plt
-from collections import Counter
 import seaborn as sns
 
 from_pbdb = ExcelFile("/Users/kristinakocakova/Dropbox/Analyses/Data/Master files/fins.xlsx")
