3eef0db5858a88f6cc0f4362d1e27ab4af1ab0e3
Kristina-K12 <kocakova.kristina@gmail.com>
2025-04-09 11:40:53 +0200

    added filtering of superorders and orders

--- code/fins_extraction_and_filtering.py
+++ code/fins_extraction_and_filtering.py
@@ -4,12 +4,23 @@ Author: Kristína Kocáková
 Description:
 Extraction of data from FINS given the chosen parameters
 """
+"""
+NOTE:
+
+When filtering the data for Selachimorph taxa, please use the following
+in order to include Cretomanta and Odontorhytis genera as Selachimorphs
+despite having an incertae sedis superorder
+
+selachis = ["Galeomorphii", "Squalomorphii"]
+occurrences = occurrences.loc[(occurrences["superorder"].isin(selachis))| (occurrences["genus"] == "Odontorhytis")| (occurrences["genus"] == "Cretomanta")]
+
+"""
 
 import os
 from pandas import *
 from collections import Counter
 
-fins = ExcelFile("/Users/kristinakocakova/Dropbox/Analyses/Data/Master files/fins.xlsx")
+fins = ExcelFile("/Volumes/External_memory/Dropbox/FINS dataset/Data/Master files/fins.xlsx")
 occurrences = read_excel(fins, "Occurrences")
 
 # select only valid entries (considering both age and taxonomy)
@@ -19,22 +30,22 @@ occurrences = occurrences.loc[occurrences["taxonomy_validation"] == "valid"]
 
 # select only occurrences identified to a species or genus level
 
-occurrences = occurrences.loc[(occurrences["rank"] == "species") | (occurrences["rank"] == "genus")]
 occurrences = occurrences.loc[occurrences["early_interval"] != "present"]
 
 # only occurrences with age resolution below 15 Myr
 occurrences = occurrences.loc[occurrences["age_range"] <= 15]
 
-# select columns - locality id (i.e. fossil site), species and genus name, paleocoordinates, minimum and maximum age, epoch (early_ and late_epoch columns are identical for occurrences with the selected age resolution)
-
-occurrences = occurrences[["locality_id", "accepted_name", "genus", "rank", "max_ma", "min_ma", "early_epoch", "paleolat", "paleolon", "paleoocean"]]
-occurrences = occurrences.rename(columns={"early_epoch": "epoch"})
-
 # drop duplicates (Optional, occurrences of the same taxon from the same locality with identical ages could represent the same individual or population)
-occurrences = occurrences.drop_duplicates(keep="first")
+occurrences = occurrences.drop_duplicates(subset=["collection_no", "accepted_name"], keep="first")
+
+# OPTIONAL: Only occurrences belonging to a specific order
+order = occurrences.loc[occurrences["order"] == "Squaliformes"]
 
 # extract only species
-species = occurrences.loc[occurrences["rank"] == "species"]
+species = order.loc[order["rank"] == "species"]
+
+# OR extract genera
+genera = occurrences.loc[(occurrences["rank"] == "species") | (occurrences["rank"] == "genus")]
 
 # OPTIONAL: exclude singletons
 
@@ -48,7 +59,5 @@ for i in counter:
 species_no_sing = species[~species["accepted_name"].isin(singletons)]
 
 # save files
-cwd = os.getcwd()
-occurrences.to_csv(cwd + "/data/species_genus_input.txt", sep = "\t", index = False)
-species.to_csv(cwd + "/data/species_input.txt", sep = "\t", index = False)
-species_no_sing.to_csv(cwd + "/data/species_input_no_sing.txt", sep = "\t", index = False)
+species.to_excel("/Volumes/External_memory/Dropbox/FINS dataset/Data/Master files/data_for_analyses/orders_filtered/squaliformes.xlsx", index = False)
+
